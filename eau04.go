109cef839735a577d580d203bc6b95deaeffd624
Henrysoto <rtcwpub@gmail.com>
2023-06-16 12:11:54 +0200

    eau04 OK

--- eau04.go
+++ eau04.go
@@ -10,7 +10,6 @@ func main() {
 	if len(os.Args) == 2 {
 		fibonacci := []int64{0, 1}
 		arg, _ := strconv.ParseInt(os.Args[1], 10, 64)
-
 		if arg >= 0 {
 			for i := int64(2); i <= arg; i++ {
 				fibonacci = append(fibonacci, fibonacci[i-1]+fibonacci[i-2])
