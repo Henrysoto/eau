6c3888331c92ebd3da8dc9174251d2b0c38dfdb1
Henrysoto <rtcwpub@gmail.com>
2023-06-16 11:28:29 +0200

    eau02 OK

---
+++ eau02.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 0; i <= 20; i++ {
+		for j := 0; j <= 20; j++ {
+			if i < j {
+				fmt.Printf("%02d %02d\t", i, j)
+			}
+		}
+	}
+	fmt.Println()
+}
