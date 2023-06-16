e8bf0221547366e65b13bea8cc7e1e19fd538117
Henrysoto <rtcwpub@gmail.com>
2023-06-16 11:29:05 +0200

    eau02 OK

--- eau02.go
+++ eau02.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	for i := 0; i <= 20; i++ {
-		for j := 0; j <= 20; j++ {
+	for i := 0; i <= 99; i++ {
+		for j := 0; j <= 99; j++ {
 			if i < j {
 				fmt.Printf("%02d %02d\t", i, j)
 			}
