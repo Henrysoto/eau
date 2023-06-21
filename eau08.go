7c6fbab91a2b26dcbdc02453da7929cbe59f977d
Henrysoto <rtcwpub@gmail.com>
2023-06-21 12:25:23 +0200

    eau08 OK

--- eau08.go
+++ eau08.go
@@ -4,16 +4,19 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"unicode"
 )
 
 func main() {
 	if len(os.Args) == 2 {
-		str := ""
-		for _, c := range strings.Fields(os.Args[1]) {
-			str = fmt.Sprintf("%s %s%s", str, strings.ToUpper(string(c[0])), c[1:])
+		if unicode.IsLetter(rune(os.Args[1][0])) {
+			str := ""
+			for _, c := range strings.Fields(os.Args[1]) {
+				str = fmt.Sprintf("%s %s%s", str, strings.ToUpper(string(c[0])), c[1:])
+			}
+			fmt.Println(str)
+			return
 		}
-		fmt.Println(str)
-	} else {
-		fmt.Println("erreur.")
 	}
+	fmt.Println("error")
 }
