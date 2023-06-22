eb7eecb22a9934bceba21fc6145f9a59622183d9
Henrysoto <rtcwpub@gmail.com>
2023-06-22 15:29:14 +0200

    ajout des consignes

--- eau03.go
+++ eau03.go
@@ -5,6 +5,27 @@ import (
 	"os"
 )
 
+/*
+Créez un programme qui affiche ses arguments reçus à l’envers.
+
+
+Exemples d’utilisation :
+$> python exo.py “Suis” “Je” “Drôle”
+Drôle
+Je
+Suis
+
+
+$> python exo.py ha ho
+ho
+ha
+
+$> python exo.py “Bonjour 36”
+Bonjour 36
+
+Afficher error et quitter le programme en cas de problèmes d’arguments.
+*/
+
 func main() {
 	if len(os.Args) >= 2 {
 		for i := len(os.Args) - 1; i > 0; i-- {
