d7ed7b253774e4b853845807027f2577f103b657
Henrysoto <rtcwpub@gmail.com>
2023-06-22 16:06:27 +0200

    eau15 OK

--- eau15.go
+++ eau15.go
@@ -6,6 +6,21 @@ import (
 	"sort"
 )
 
+/*
+Créez un programme qui trie les éléments donnés en argument par ordre ASCII.
+
+
+Exemples d’utilisation :
+$> python exo.py Alfred Momo Gilbert
+Alfred Gilbert Momo
+
+
+$> python exo.py A Z E R T Y
+A E R T Y Z
+
+Afficher error et quitter le programme en cas de problèmes d’arguments.
+*/
+
 func main() {
 	if len(os.Args) >= 3 {
 		arr := os.Args[1:]
